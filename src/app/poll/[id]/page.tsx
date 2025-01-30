76ed63081bc46cdcc7197f74d8b49efbe2366fad
Dylan <djlafo@gmail.com>
2025-01-29 18:03:13 -0600

    last minute css tweak

--- src/app/poll/[id]/page.tsx
+++ src/app/poll/[id]/page.tsx
@@ -3,6 +3,8 @@ import Poll from './Poll';
 import { Suspense } from "react";
 import { Metadata } from "next";
 
+import styles from './poll.module.css';
+
 export const metadata: Metadata = {
     title: "Poll"
 };
@@ -11,9 +13,11 @@ interface PageProps {
     params : Promise<{ id: string}>
 }
 export default function Page(props: PageProps) {
-    return <Suspense fallback='Loading poll...'>
-        <PollLoader params={props.params}/>
-    </Suspense>;
+    return <div className={styles.poll}>
+        <Suspense fallback='Loading poll...'>
+            <PollLoader params={props.params}/>
+        </Suspense>
+    </div>;
 }
 
 async function PollLoader(props: PageProps) {
--- src/app/poll/page.tsx
+++ src/app/poll/page.tsx
@@ -1,22 +1,28 @@
-import { listPolls, SerializedPoll } from "@/actions/polls";
+import { Suspense } from "react";
 import { Metadata } from "next";
 
+import { listPolls } from "@/actions/polls";
+
 import PollButton from './PollButton';
-import { Suspense } from "react";
+
+import styles from './polllist.module.css';
 
 export const metadata: Metadata = {
     title: "Polls"
 };
 
 export default function Page() {
-    return <Suspense fallback='Loading polls...'>
-        <PollList/>
-    </Suspense>
+    return <div className={styles.polllist}>
+        <Suspense fallback='Loading polls...'>
+            <PollList/>
+        </Suspense>
+    </div>;
 }
 
 async function PollList() {
     const polls = await listPolls();
     return <div>
+        This section is WIP!
         {
             polls && polls.map(p => {
                 return <div key={p.uuid}>
