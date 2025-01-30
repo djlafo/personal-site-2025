b20eba60ced22532e2f5a26048e853cf475c7e8b
Dylan <djlafo@gmail.com>
2025-01-30 16:59:50 -0600

    woops

--- drizzle/0000_fearless_mach_iv.sql
+++ drizzle/0000_fearless_mach_iv.sql
@@ -1,43 +1,43 @@
--- CREATE TABLE "planner" (
--- 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "planner_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
--- 	"data" json
--- );
--- --> statement-breakpoint
--- CREATE TABLE "poll_options" (
--- 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "poll_options_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
--- 	"poll_uuid" varchar(255) NOT NULL,
--- 	"user_id" integer,
--- 	"text" varchar(255) NOT NULL,
--- 	"active" boolean DEFAULT true
--- );
--- --> statement-breakpoint
--- CREATE TABLE "poll_votes" (
--- 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "poll_votes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
--- 	"poll_option_id" integer NOT NULL,
--- 	"user_id" integer,
--- 	"ip" varchar(255) NOT NULL
--- );
--- --> statement-breakpoint
--- CREATE TABLE "polls" (
--- 	"uuid" varchar(255) PRIMARY KEY NOT NULL,
--- 	"user_id" integer NOT NULL,
--- 	"title" varchar(255) NOT NULL,
--- 	"guest_addable" boolean DEFAULT false NOT NULL,
--- 	"date_created" date DEFAULT now() NOT NULL,
--- 	"active" boolean DEFAULT true
--- );
--- --> statement-breakpoint
--- CREATE TABLE "users" (
--- 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
--- 	"username" varchar(255) NOT NULL,
--- 	"password" varchar(255) NOT NULL,
--- 	"planner_id" integer,
--- 	CONSTRAINT "users_username_unique" UNIQUE("username")
--- );
--- --> statement-breakpoint
--- ALTER TABLE "poll_options" ADD CONSTRAINT "poll_options_poll_uuid_polls_uuid_fk" FOREIGN KEY ("poll_uuid") REFERENCES "public"."polls"("uuid") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE "poll_options" ADD CONSTRAINT "poll_options_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE "poll_votes" ADD CONSTRAINT "poll_votes_poll_option_id_poll_options_id_fk" FOREIGN KEY ("poll_option_id") REFERENCES "public"."poll_options"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE "poll_votes" ADD CONSTRAINT "poll_votes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE "polls" ADD CONSTRAINT "polls_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- ALTER TABLE "users" ADD CONSTRAINT "users_planner_id_planner_id_fk" FOREIGN KEY ("planner_id") REFERENCES "public"."planner"("id") ON DELETE no action ON UPDATE no action;
+CREATE TABLE "planner" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "planner_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"data" json
+);
+--> statement-breakpoint
+CREATE TABLE "poll_options" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "poll_options_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"poll_uuid" varchar(255) NOT NULL,
+	"user_id" integer,
+	"text" varchar(255) NOT NULL,
+	"active" boolean DEFAULT true
+);
+--> statement-breakpoint
+CREATE TABLE "poll_votes" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "poll_votes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"poll_option_id" integer NOT NULL,
+	"user_id" integer,
+	"ip" varchar(255) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "polls" (
+	"uuid" varchar(255) PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"guest_addable" boolean DEFAULT false NOT NULL,
+	"date_created" date DEFAULT now() NOT NULL,
+	"active" boolean DEFAULT true
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"username" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"planner_id" integer,
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+ALTER TABLE "poll_options" ADD CONSTRAINT "poll_options_poll_uuid_polls_uuid_fk" FOREIGN KEY ("poll_uuid") REFERENCES "public"."polls"("uuid") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "poll_options" ADD CONSTRAINT "poll_options_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "poll_votes" ADD CONSTRAINT "poll_votes_poll_option_id_poll_options_id_fk" FOREIGN KEY ("poll_option_id") REFERENCES "public"."poll_options"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "poll_votes" ADD CONSTRAINT "poll_votes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "polls" ADD CONSTRAINT "polls_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_planner_id_planner_id_fk" FOREIGN KEY ("planner_id") REFERENCES "public"."planner"("id") ON DELETE no action ON UPDATE no action;
