2b8c0d0f036d3cb7fead39a890e1da013d0a86c0
Dylan <djlafo@gmail.com>
2025-02-14 13:36:21 -0600

    Show auth timer

--- src/components/HeaderBar/LoginButton.tsx
+++ src/components/HeaderBar/LoginButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { logout } from '@/actions/auth';
 
@@ -20,10 +20,46 @@ export default function LoginButton() {
         });
     }
 
+
+
     return <div className={styles.loginbutton}>
         {
-            (!pending && !!user && <span>{user.username} <input type='button' value='Logout' onClick={() => _logout()}/></span>) ||
-            (!pending && !user && <input type='button' value='Login' onClick={() => router.push('/login')}/>)
+            (!pending && !!user && 
+                <span>
+                    {user.username}&nbsp;
+                    <Timer endTime={user.exp}/>&nbsp;
+                    <input type='button' 
+                        value='Logout'
+                        onClick={() => _logout()}/>
+                </span>) 
+            ||
+            (!pending && !user && 
+                <input type='button' 
+                    value='Login' 
+                    onClick={() => router.push('/login')}/>)
         }
     </div>
+}
+
+function Timer({endTime}: {endTime: number}) {
+    const [timeInterval, setTimeInterval] = useState<number | null>(null);
+    const [, setTimeFlipper] = useState(false);
+
+    useEffect(() => {
+        const timerHandler: TimerHandler = () => {
+            setTimeFlipper(tf => !tf);
+        }
+        setTimeInterval(setInterval(timerHandler, 60000));
+        return () => {
+            if(!timeInterval) return;
+            clearInterval(timeInterval);
+            setTimeInterval(null);
+        }
+    }, [endTime]);
+
+    const timeLeft = Math.floor(endTime-(Number(new Date())/1000));
+    const hours = Math.floor(timeLeft/60/60);
+    const minutes = Math.floor(timeLeft/60 - (hours*60));
+
+    return <>{hours}:{minutes.toString().padStart(2, '0')}</>
 }
