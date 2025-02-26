864310b70541eb7df6f8662ad0112af05c70cd24
Dylan <djlafo@gmail.com>
2025-02-26 09:04:38 -0600

    fix weather woops

--- src/app/weather/[zip]/page.tsx
+++ src/app/weather/[zip]/page.tsx
@@ -2,25 +2,18 @@
 
 import { redirect } from "next/navigation";
 import { getCoordsFromZip } from "../location";
-import { LoadingScreenFallBack } from "@/components/LoadingScreen";
-import { Suspense } from "react";
 
 interface PageProps {
     params: Promise<{ zip: string }>;
 }
 export default async function Page({ params }: PageProps) {
     const zip = (await params).zip;
-    return <Suspense fallback={<LoadingScreenFallBack/>}>
-        <LoadAndRedirect zip={zip}/>
-    </Suspense>;
-}
-
-async function LoadAndRedirect({ zip }: {zip: string}) {
+    let coords = '';
     try {
-        const coords = await getCoordsFromZip(zip);
-        redirect(`/weather/${zip}/${coords}`);
+        coords = await getCoordsFromZip(zip);
     } catch {
         redirect('/weather');
+    } finally {
+        redirect(`/weather/${zip}/${coords}`);
     }
-    return <></>;
 }
