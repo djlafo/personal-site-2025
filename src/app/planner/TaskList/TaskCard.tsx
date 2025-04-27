9eec84d551cc3ef5ca4301d3c8fd99836f95532f
Dylan <djlafo@gmail.com>
2025-04-27 18:00:46 -0500

    css tweak

--- src/app/planner/TaskList/TaskCard.tsx
+++ src/app/planner/TaskList/TaskCard.tsx
@@ -26,7 +26,7 @@ export default function TaskCard({ task, onSetEdit }: TaskCardProps) {
                 <button onClick={onSetEdit}>Edit</button>
                 <span>{task.label || 'No Label'}</span>
             </div>
-            <div>
+            <div className={styles.motHeader}>
                 {task.text && <Image src='/icons/phone.png' alt='Text Enabled' width={18} height={30}/>}
                 <span className={styles.motivation}>{task.motivation}</span>
             </div>
