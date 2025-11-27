6947ac4256f0cbabd6451124781a0908d342580c
Dylan <djlafo@gmail.com>
2025-11-27 09:45:03 -0600

    Fix everything

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,33 +1,29 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 import type { Config } from 'drizzle-kit';
-import type { ConnectionOptions } from 'tls';
+import { ConnectionOptions } from 'node:tls';
 
 interface sslOpts {
   ca?: string;
 }
 
-const dbCreds : {
+export const dbCreds : {
   host: string;
   port: number;
   user: string;
   password: string;
   database: string;
-  ssl?: ConnectionOptions;
+  ssl?: ConnectionOptions | "prefer";
 } = {
   host: process.env.DATABASE_HOST!,
   port: parseInt(process.env.DATABASE_PORT!),
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
-  database: process.env.DATABASE!
-};
-
-if(process.env.DATABASE_CA) {
-  const sslOpts: ConnectionOptions = {
+  database: process.env.DATABASE!,
+  ssl: process.env.DATABASE_CA ? {
     ca: process.env.DATABASE_CA!
-  };
-  dbCreds.ssl = sslOpts;
-}
+  } : "prefer"
+};
 
 const config: Config = {
   out: './drizzle',
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,22 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { ConnectionOptions } from 'node:tls';
 
-const db = drizzle(process.env.DATABASE_URL!);
+interface connectionInfo {
+    connectionString: string,
+    ssl?: ConnectionOptions
+}
+
+const config : {
+    connection: connectionInfo
+} = {
+    connection: {
+        connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`,
+        ssl: process.env.DATABASE_CA ? {
+            ca: process.env.DATABASE_CA
+        } : undefined
+    }
+};
+
+const db = drizzle(config);
 export default db;
