405defb031e4a30a3732e9391c9eb8b30f320df7
Dylan <djlafo@gmail.com>
2025-11-27 07:15:50 -0600

    Add CA cert to drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,35 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
+import type { ConnectionOptions } from 'tls';
 
-export default defineConfig({
+interface sslOpts {
+  ca?: string;
+}
+
+const dbCreds : {
+  url: string;
+  ssl?: ConnectionOptions;
+} = {
+  url: process.env.DATABASE_URL!,
+};
+
+if(process.env.DATABASE_CA) {
+  const sslOpts: ConnectionOptions = {
+    ca: process.env.DATABASE_CA!
+  };
+  dbCreds.ssl = sslOpts;
+}
+
+const config: Config = {
   out: './drizzle',
   schema: './src/db/schema',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+  dbCredentials: dbCreds,
   migrations: {
     table: 'migrations',
     schema: 'public'
   }
-});
+};
+
+export default defineConfig(config);
